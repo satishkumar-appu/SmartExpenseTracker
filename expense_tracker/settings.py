bf5e18205366daa61aae4eb601f858d6d8558b2b
satish-appu143 <amudalanandini7@gmail.com>
2026-07-21 22:00:26 +0530

    Switch from MySQL to PostgreSQL for deployment

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -116,23 +116,14 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # Database
 
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.mysql',
-
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
-
         'USER': config('DB_USER'),
-
         'PASSWORD': config('DB_PASSWORD'),
-
         'HOST': config('DB_HOST'),
-
         'PORT': config('DB_PORT'),
-
     }
-
 }
 
 
